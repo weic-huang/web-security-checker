8afca0c9d0a264a1b2b92a2319dd25937e2be56f
peter9503@gmail.com <peter9503@gmail.com>
2019-11-23 20:10:54 +0800

    init SourecodeHandler

---
+++ src/SourceCodeHanler/SourceCodeHandler.py
@@ -0,0 +1,24 @@
+class SourceCodeHandler():
+	def __init__(self,url):
+		self.url = url
+		self.sourceCode = ""
+		self.HtmlObject = {}
+		self.JSfunctions = []
+		self.srcPaths = []
+
+	def parse(self):
+		# use self.url to build other attribute
+		pass
+
+	def callMiningChecker(self):
+		# as title
+		pass
+
+	def callJSScanner(self):
+		# as title
+		pass
+
+	def callHtmlScanner(self):
+		# as title
+		pass
+
