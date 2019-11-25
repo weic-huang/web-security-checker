b0f96ef160c9ea4598ef97780f655b0aad8ad197
HsiaoIJen <s123unny@gmail.com>
2019-11-25 16:12:41 +0800

    init browser simulator

---
+++ src/BrowserSimulator/BrowserSimulator.py
@@ -0,0 +1,16 @@
+
+class BrowserSimulator():
+	def __init__(self, url):
+		self.url = url
+	
+	def simulateManager(self):
+		pass
+	
+	def simulator(self):
+		pass
+	
+	def getUsage(self):
+		pass
+	
+	def getView(self):
+		pass
