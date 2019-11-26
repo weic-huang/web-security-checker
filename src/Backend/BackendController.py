149196a97bf8a46def84e255c6f1c9b4467143ff
ziyuan <superboy753@yahoo.com.tw>
2019-11-26 10:30:46 +0800

    Backend structure

---
+++ src/Backend/BackendController.py
@@ -0,0 +1,16 @@
+import Result
+
+class BackendController():
+	def __init__(self):
+		self.Result = Result()
+
+	def listenSocket():
+		#socket listener
+		pass
+
+	def sendSocker():
+		#send socket to frontend
+		pass
+	def buildThread():
+		#build thread to do subwork
+		pass
