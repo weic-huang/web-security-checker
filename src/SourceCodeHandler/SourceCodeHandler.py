bd6c0b78207392845214a5708e11016b05dbc28e
peter9503@gmail.com <peter9503@gmail.com>
2019-12-08 23:58:02 +0800

    update sourceCodeHandler

--- src/SourceCodeHandler/SourceCodeHandler.py
+++ src/SourceCodeHandler/SourceCodeHandler.py
@@ -1,4 +1,10 @@
 # need to add some import files
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import validators
+
 
 class SourceCodeHandler():
 	def __init__(self,url):
@@ -10,7 +16,16 @@ class SourceCodeHandler():
 
 	def parse(self):
 		# use self.url to build other attribute
-		pass
+		options = Options();
+		options.add_argument("--headless");
+
+		driver = webdriver.Chrome(chrome_options=options,executable_path="./include/chromedriver.exe");
+		driver.get(self.url)
+
+		soup = BeautifulSoup(driver.page_source, "html.parser")
+		srcs = [i.get('src') for i in soup.find_all('script') if i.get('src')]
+
+
 
 	def callMiningChecker(self):
 		# as title
@@ -23,4 +38,3 @@ class SourceCodeHandler():
 	def callHtmlScanner(self):
 		# as title
 		pass
-
