6b2dfea92fb91da0ad42d2ddacf0e4f0c0292a08
luicdayi <luicdawang0708@gmail.com>
2019-12-17 20:58:28 +0800

    fix views bug

--- src/WebApplication/WebApp/views.py
+++ src/WebApplication/WebApp/views.py
@@ -8,7 +8,7 @@ class Frontend():
 	def __init__(self):
 		self.url_mode = 0
 		self.url_alert = 0
-		self.model=Model()
+		self.model=Model("")
 		print("init test")
 
 	def index(self,request):
@@ -18,7 +18,7 @@ class Frontend():
 			print("change to back");
 			print("mode: ",self.url_mode );
 			print("alert: ",self.url_alert );
-			url = "" #self.model.validURL(enterurl);
+			url = self.model.validURL(enterurl);
 			if url=="": #get wrong url
 				self.url_mode = 0
 				self.url_alert = 1
@@ -29,7 +29,7 @@ class Frontend():
 			print(self.url_alert );
 		
 
-		if self.url_mode==1:
+		if self.url_mode==0:
 			return render(request, './indexpage.html',{'url_alert':self.url_alert,'url_output':enterurl})
 		else:
 			print("url_mode=1")
