4a3104204304cc2fa1f0ca5002e07b296d706c6f
HsiaoIJen <s123unny@gmail.com>
2019-12-17 16:37:57 +0800

    add do screenshot

--- src/BrowserSimulator/Simulate.py
+++ src/BrowserSimulator/Simulate.py
@@ -3,11 +3,12 @@ from selenium import webdriver
 import sys
 
 url = sys.argv[1]
+viewfilename = sys.argv[2]
 options = Options()
 options.add_argument('--headless')
 browser = webdriver.Chrome(chrome_options=options)
 browser.get(url)
 
-browser.save_screenshot(url)
+browser.save_screenshot(viewfilename)
 print ('done')
 sys.stdout.flush()
