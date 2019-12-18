67de041b56443c4a2bfc67a6e2b082b4b4f8c2ab
luicdayi <luicdawang0708@gmail.com>
2019-12-18 13:27:42 +0800

    fixed frontend

--- src/WebApplication/WebApp/views.py
+++ src/WebApplication/WebApp/views.py
@@ -9,10 +9,11 @@ class Frontend():
 		self.url_mode = 0
 		self.url_alert = 0
 		self.model=Model("")
+		self.url=""
 		
 
 	def index(self,request):
-
+		self.url_mode = 0
 		enterurl = request.POST.get('URL_input') 
 		if enterurl!=None: # type url 
 			
@@ -22,17 +23,21 @@ class Frontend():
 			if url=="": #get wrong url
 				self.url_mode = 0
 				self.url_alert = 1
+				self.url=""
 			else : #get correct url
 				self.url_mode = 1
 				self.url_alert = 0
-				self.model.urlProcess(url)
-			
+				self.url=enterurl
+		else:
+			self.url_alert=0
+
+
 		if self.url_mode==0:
 			return render(request, './indexpage.html',{'url_alert':self.url_alert,'url_output':enterurl})
-		else:
+		else :
 			Result=self.model.get_result()
 			return render(request, './resultpage.html',{
-					'url':Result.Url,
+					'url':self.url,
 					'BlacklistResult':Result.BlackListManager,
 					'BrowserSimulatorResult':Result.BrowserSimulator,
 					'SourceCodeHandler':Result.SourceCodeHandler,
