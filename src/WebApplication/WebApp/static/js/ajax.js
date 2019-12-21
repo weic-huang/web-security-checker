decf9c5a87c47801d637a7907c2c417b517db29c
luicdayi <luicdawang0708@gmail.com>
2019-12-21 12:08:43 +0800

    add unit

--- src/WebApplication/WebApp/static/js/ajax.js
+++ src/WebApplication/WebApp/static/js/ajax.js
@@ -83,7 +83,7 @@ var x_icon= "<img src=\"/static/img/x.png\" alt=\"safe\" width=\"20px\" height=\
                
                 }
                 else if (i=="mem"){
-                  if (data[i]>1000)
+                  if (data[i]>200)
                     document.getElementById("BS_"+i+"_icon").innerHTML = warning_icon;
                   else
                     document.getElementById("BS_"+i+"_icon").innerHTML = safe_icon;
